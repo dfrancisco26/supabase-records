ee943519777454fa376058d26c01f2ea806b48a9
David Francisco <dfrancisco2677@gmail.com>
2022-05-15 20:15:14 -0700

    Added fetch utils and refactored app js function to exe on load, but await fetch.

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
 // import functions and grab DOM elements
-import { records } from './records-data.js';
+import { getRecords } from './fetch-utils.js';
 import { simpleRender } from './utils.js';
 
 // let state
-for (let record of records) {
-    const recordsDiv = document.getElementById('records');
 
-    const recordDiv = simpleRender(record);
-    recordsDiv.append(recordDiv);
-}
+window.addEventListener('load', async () => {
+    const records = await getRecords();
+    for (let record of records) {
+        const recordsDiv = document.getElementById('records');
+        const recordDiv = simpleRender(record);
+        recordsDiv.append(recordDiv);
+
+    }
+});
+
+    
 // set event listeners 
   // get user input
   // use user input to update state 
